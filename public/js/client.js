2f6b2d50f37512ca3d1ceabe074f7df30a5cf4a7
mikemorris <mike@nclud.com>
2012-11-09 17:10:06 -0500

    extracted control interface into gyrocopter module, added multidirectional functionality

--- public/js/client.js
+++ public/js/client.js
@@ -1,67 +1,5 @@
 var game = {} || game;
 
-var keymap = [
-  {
-    'keys': {
-      'keyCode': 87,
-      'altKey': false,
-      'ctrlKey': false,
-      'shiftKey': false
-    },
-    'command': 'forward'
-  },
-  {
-    'keys': {
-      'keyCode': 65,
-      'altKey': false,
-      'ctrlKey': false,
-      'shiftKey': false
-    },
-    'command': 'left'
-  },
-  {
-    'keys': {
-      'keyCode': 83,
-      'altKey': false,
-      'ctrlKey': false,
-      'shiftKey': false
-    },
-    'command': 'reverse'
-  },
-  {
-    'keys': {
-      'keyCode': 68,
-      'altKey': false,
-      'ctrlKey': false,
-      'shiftKey': false
-    },
-    'command': 'right'
-  }
-];
-
-var map = [
-  {
-    'axis': 'b',
-    'positive': true,
-    'command': 'forward'
-  },
-  {
-    'axis': 'b',
-    'positive': false,
-    'command': 'reverse'
-  },
-  {
-    'axis': 'g',
-    'positive': false,
-    'command': 'left'
-  },
-  {
-    'axis': 'g',
-    'positive': true,
-    'command': 'right'
-  }
-];
-
 (function() {
   var socket;
 
@@ -70,68 +8,10 @@ var map = [
     y: 0
   };
 
-  var gyroscope;
-  var accelerometer;
-
   var queue = {};
   queue.move = [];
   queue.physics = [];
   queue.animation = [];
-
-  // get command from keybindings
-  var binding = function(event) {
-    var bind = _.find(keymap, function(keybind) {
-      return keybind.keys.keyCode === event.keyCode &&
-        keybind.keys.altKey === event.altKey &&
-        keybind.keys.ctrlKey === event.ctrlKey &&
-        keybind.keys.shiftKey === event.shiftKey;
-    });
-
-    if (bind !== undefined) {
-      return bind.command;
-    }
-  };
-
-  // get command from motion bindings
-  var motion = function() {
-    var last = queue.move.length - 1;
-    var now = queue.move[0].gyroscope;
-    var old = queue.move[last].gyroscope;
-
-    var delta = [
-      {
-        key: 'a',
-        value: now.alpha - old.alpha
-      },
-      {
-        key: 'b',
-        value: now.beta - old.beta
-      },
-      {
-        key: 'g',
-        value: now.gamma - old.gamma
-      }
-    ];
-
-    var max = _.max(delta, function(axis) {
-      var abs = Math.abs(axis.value);
-
-      if (abs > 1) {
-        return abs;
-      }
-    });
-
-    if (max !== undefined) {
-      var bind = _.find(map, function(binding) {
-        return binding.axis === max.key &&
-          binding.positive === (max.value > 0);
-      });
-
-      if (bind !== undefined) {
-        return bind.command;
-      }
-    }
-  };
 
   // TODO: replace with physics logic using dependency injection pattern
   var valid = function(command) {
@@ -141,59 +21,21 @@ var map = [
   };
 
   var physics = function() {
-    if (gyroscope && accelerometer) {
-      // create move object and add to physics queue
-      var move = {
-        time: Date.now(),
-        gyroscope: gyroscope,
-        accelerometer: accelerometer,
-        state: state
-      };
+    // create move object and add to physics queue
+    var move = {
+      time: Date.now(),
+      state: state
+    };
 
-      // add move to queue, then trim queue to max size
-      queue.move.unshift(move);
-      queue.move = queue.move.slice(0, 10);
-
-      // if motion, send command
-      var command = motion();
-      if (command !== undefined) {
-        queue.physics.push({ data: command });
-      }
-
-      // update gyroscope display
-      var a = Math.round(gyroscope.alpha);
-      var b = Math.round(gyroscope.beta);
-      var g = Math.round(gyroscope.gamma);
-
-      // add plus sign to string for positive numbers
-      if(a >= 0) { a = '+' + a}
-      if(b >= 0) { b = '+' + b}
-      if(g >= 0) { g = '+' + g}
-      
-      $('#gyroscope .alpha .value').text(a);
-      $('#gyroscope .beta .value').text(b);
-      $('#gyroscope .gamma .value').text(g);
-
-      // update accelerometer display
-      var x = Math.round(accelerometer.acceleration.x);
-      var y = Math.round(accelerometer.acceleration.y);
-      var z = Math.round(accelerometer.acceleration.z);
-
-      // add plus sign to string for positive numbers
-      if(x >= 0) { x = '+' + x}
-      if(y >= 0) { y = '+' + y}
-      if(z >= 0) { z = '+' + z}
-      
-      $('#accelerometer .x .value').text(x);
-      $('#accelerometer .y .value').text(y);
-      $('#accelerometer .z .value').text(z);
-    }
+    // add move to queue, then trim queue to max size
+    queue.move.unshift(move);
+    queue.move = queue.move.slice(0, 10);
 
     while (queue.physics.length) {
       var command = valid(queue.physics.shift());
 
       if (command !== undefined) {
-        switch(command.data) {
+        switch(command) {
           case 'forward':
             state.x++;
             break;
@@ -208,7 +50,6 @@ var map = [
             break;
         }
 
-        //queue.animation.push(command);
         queue.animation.push(state);
 
         // console.log(command);
@@ -245,35 +86,39 @@ var map = [
       $('#conversation').append('<p><strong>' + username + ':</strong> ' + data + '</p>');
     });
 
-    socket.on('command:update', function (username, data) {
-      //queue.animation.push({ data: data, username: username });
-    });
-
     socket.on('state:update', function (data) {
       state = data;
       queue.animation.push(data);
     });
 
-    // keybindings
-    // TODO: switch input methods from push to pull?
-    $(document).keydown(function(event) {
-      // console.log(event);
-      var command = binding(event);
+    socket.emit('user:add', 'You');
 
-      if (command !== undefined) {
-        queue.physics.push({ data: command });
-      }
+    document.addEventListener('gyrocopter', function(event) {
+      queue.physics.push(event.detail.direction);
     });
 
-    window.ondeviceorientation = function(event) {
-      gyroscope = event;
-    }
+    // gyroscope
+    var g = Gyrocopter.factory({
+      'constructor': 'gyroscope', // module.constructor.gyroscope
+      'map': [
+        [], // [ alphaPositive, alphaNegative ]
+        ['forward', 'reverse'], // [ betaPositive, betaNegative ]
+        ['left', 'right'] // [ gammaPositive, gammaNegative ]
+      ],
+      'func': 'motion' // module.motion
+    });
 
-    window.ondevicemotion = function(event) {
-      accelerometer = event;
-    }
-
-    socket.emit('user:add', 'You');
+    // keyboard
+    var k = Gyrocopter.factory({
+      'constructor': 'keyboard', // module.constructor.keyboard
+      'map': {
+        'forward': [87, 38], // [ W, Up ]
+        'reverse': [83, 40], // [ S, Down ]
+        'left': [65, 37], // [ A, Left ]
+        'right': [68, 39] // [ D, Right ]
+      },
+      'func': 'keyboard'
+    });
 
     // init physics loop, fixed time step in milliseconds
     setInterval(physics, 15);
---
+++ public/js/gyrocopter.js
@@ -0,0 +1,185 @@
+(function (window, undefined) {
+  var Gyrocopter = (function(module) {
+    // protected variables
+    var document = window.document;
+    var init = false;
+    var gyroscope;
+    var accelerometer;
+    var keyboard = [];
+
+    var queue = {};
+    queue.motion = [];
+    queue.keyboard = [];
+
+    // public variables
+    module.TIME_STEP = 15;
+
+    // public functions
+    module.motion = function(map) {
+      // create input object and add to motion queue
+      var input = {
+        time: Date.now(),
+        gyroscope: gyroscope,
+        accelerometer: accelerometer
+      };
+
+      // add input to queue, then trim queue to max size
+      queue.motion.unshift(input);
+      queue.motion = queue.motion.slice(0, 10);
+
+      var last = queue.motion.length - 1;
+      var now = queue.motion[0].gyroscope;
+      var old = queue.motion[last].gyroscope;
+
+      // alpha, beta, gamma
+      var delta = [
+        now.alpha - old.alpha,
+        now.beta - old.beta,
+        now.gamma - old.gamma
+      ];
+
+      var length = delta.length;
+      for (var i; i < length; i++) {
+        var value = delta[i];
+        if (Math.abs(value) > 1) {
+          var index = (value >= 0 ? 0 : 1);
+          var event = map[i][index];
+
+          if (event) {
+            document.dispatchEvent(new CustomEvent('gyrocopter', {
+              detail: {
+                direction: event
+              }
+            }));
+          }
+        }
+      }
+    };
+
+    module.keyboard = function(map) {
+      // create input object and add to keyboard queue
+      var input = {
+        time: Date.now(),
+        keyboard: keyboard
+      };
+
+      // add input to queue, then trim queue to max size
+      queue.keyboard.unshift(input);
+      queue.keyboard = queue.keyboard.slice(0, 10);
+
+      var keys = Object.keys(map);
+      var length = keys.length;
+      for (var i = 0; i < length; i++) {
+        var event = keys[i];
+        var keydown = (function() {
+          var keybind = map[event];
+          for (var j = 0; j < keybind.length; j++) {
+            var key = keybind[j];
+            if (queue.keyboard[0]['keyboard'][key]) {
+              return true;
+            }
+          }
+        })();
+
+        if (keydown) {
+          document.dispatchEvent(new CustomEvent('gyrocopter', {
+            detail: {
+              direction: event
+            }
+          }));
+        }
+      }
+    };
+
+    // constructor
+    module.constructor = {};
+
+    module.constructor.gyroscope = function(callback) {
+      window.ondeviceorientation = function(event) {
+        gyroscope = event;
+      };
+
+      window.ondevicemotion = function(event) {
+        accelerometer = event;
+      };
+
+      if (typeof callback === 'function') {
+        callback();
+      }
+    };
+
+    module.constructor.keyboard = function(callback) {
+      window.onkeydown = function(event) {
+        var key = event.which || event.keyCode;
+        keyboard[key] = true;
+      };
+
+      window.onkeyup = function(event) {
+        var key = event.which || event.keyCode;
+        keyboard[key] = false;
+      };
+
+      if (typeof callback === 'function') {
+        callback();
+      }
+    };
+
+    // factory
+    module.factory = (function() {
+      var Worker = function(options) {
+        var self = this;
+        self.args = Array.prototype.slice.call(arguments[0]);
+        self.options = this.args.shift();
+
+        // options
+        self.map = self.options.map;
+        self.func = self.options.func;
+        self.step = self.options.step || module.TIME_STEP;
+
+        // constructor
+        var constructor = module.constructor[self.options.constructor];
+
+        // callback
+        var callback = self.options.callback || function() {
+          // TODO: do this in a web worker if available?
+          // init input loop, fixed time step in milliseconds
+          setInterval(function() {
+            module[self.func](self.map);
+          }, self.step);
+        };
+
+        if (constructor) {
+          constructor(callback);
+        } else if (typeof callback === 'function') {
+          callback();
+        }
+
+        return self;
+      };
+
+      return function() {
+        return new Worker(arguments);
+      };
+    })();
+
+    // return module
+    return module;
+
+  // loose augmentation
+  })(window.Gyrocopter || {});
+
+  // expose module to global scope
+  window.Gyrocopter = Gyrocopter;
+
+  // AMD module support
+  if (typeof define === 'function' && define.amd) {
+    define('gyrocopter', [], function() { return Gyrocopter; });
+  }
+})(window);
+
+/*
+window.Gyrocopter = (function(module) {
+  module.input = function() {};
+  return module;
+})(window.Gyrocopter || {});
+*/
